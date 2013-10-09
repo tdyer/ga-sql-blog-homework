29fccedd364bb95df0572d1b34e2b9b73558ed38
Tom Dyer <tdyer1@gmail.com>
2013-10-09 15:44:12 -0400

    Show al posts

--- main.rb
+++ main.rb
@@ -6,3 +6,13 @@ require_relative './setup_dml'
 
 set :server, 'webrick'
 
+get '/posts' do
+  posts = $db_conn.exec('SELECT * FROM posts;')
+  @posts.each do |post|
+    puts "post is #{post.inspect}"
+  end
+end
+
+get 'post/:id' do
+  erb :show_post
+end
--- setup_db.rb
+++ setup_db.rb
@@ -1,3 +1,4 @@
 require 'pg'
 
+# Create a global connection to the blog database
 $db_conn = PG.connect(dbname: 'blog')
