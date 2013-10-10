0930c410b364d33ed8c95598149afc6809831346
Bobby Mazzotti <robert.mazzoti@gmail.com>
2013-10-10 09:10:33 -0400

    Final changes, should of commited more

---
+++ blog.rb
@@ -0,0 +1,24 @@
+require 'pry'
+require 'pg' # this is our database gem
+require 'sinatra'
+require "sinatra/reloader" if development?
+
+configure do
+	CONN = PG.connect(dbname: 'ga-blog', host: 'localhost')
+end
+
+def query(sql)
+	CONN.exec(sql)
+end
+
+get '/' do
+	@blog = query("SELECT * FROM blog")
+	erb :blog_index
+end
+
+get '/post/:id' do
+	@blog = query("SELECT * FROM blog ")
+	@id = params[:id].to_s
+
+	erb :single_post
+end
