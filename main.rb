693b2c575e6a8aaab74e274b7b74d026c2d07780
Rahul Horé <hore.rahul@gmail.com>
2013-10-10 18:00:01 -0400

    Links to individual blog posts properly now

--- main.rb
+++ main.rb
@@ -11,7 +11,23 @@ def query(sql)
   CONNECTION.exec(sql)
 end
 
+# get all posts on one page
 get '/posts' do 
   @posts = query("SELECT * FROM posts")
   erb :posts
+end
+
+# this will be for showing the form to create new posts
+# get 'posts/new' do 
+#   erb :post_new
+# end
+
+# post '/posts/create' do 
+# end
+
+# get a single post on one page
+get '/posts/:id' do
+  post_id = params[:id]
+  @post = query("SELECT * FROM posts WHERE id = #{post_id} LIMIT 1").first
+  erb :single_post
 end
