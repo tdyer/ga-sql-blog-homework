4960c3ad0086dd357e145530c7a34f17f0e1f178
Abby Howell <abby.g.howell@gmail.com>
2013-10-09 23:22:26 -0400

    updated lorem ipsum, added second web page

--- blog_posts.rb
+++ blog_posts.rb
@@ -7,21 +7,19 @@ configure do
 	CONN = PG.connect(dbname: 'blog', host: 'localhost')
 end
 
-
 def query(sql)
 	CONN.exec(sql)
 end
-
-# binding.pry
-
-# blog_posts = [entry_1, entry_2, entry_3, entry_4, entry_5]
-
-# blog_posts.each do |post|
-# 	query("INSERT INTO blog (subject, contents, created_at) VALUES ('#{post['subject']}', '#{post['contents']}', '#{post['timestamp']}')")
-# end
 
 
 get '/blog' do
 	@result = query("SELECT * FROM blog_posts ORDER BY created_at DESC")
 	erb :blog
 end
+
+get '/blog/:id' do
+	id = params[:id]
+	@result = query("SELECT * FROM blog_posts WHERE id='#{id}'")
+    erb :full_text
+end
+
