aedc4c3992896fc3aae5631e3806ef48a45c039c
Amal Hussein <amalhussein08@gmail.com>
2013-10-10 08:24:27 -0400

    Blog complete with links to single posts...need to add CSS

--- sinatra_blog.rb
+++ sinatra_blog.rb
@@ -13,7 +13,26 @@ def query(sql)
 end 
 
 get	'/blog' do  
-	@blog = query("SELECT * FROM blog")
+	@blog = query("SELECT * FROM blog ORDER BY id")
+	@title = "Awesome Hipster Rantings"
 	erb :blog_site 
-end
+end 
+
+get '/blog/:id' do
+	id=params[:id]
+	@blog=query("SELECT * FROM blog where id = #{:id}")
+	erb :one_post
+end 
+
+
+
+# get '/blog/one_post' do 
+# @blog = query("SELECT * FROM blog")
+# @subject=params[:subject]
+# erb :one_post
+# end
+
+# posts =query("SELECT * FROM blog")
+
+#binding.pry 
 
