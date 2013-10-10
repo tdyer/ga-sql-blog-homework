d1a935c3076aa2504bbb74aa32ceabdeee1d17a2
Rahul Horé <hore.rahul@gmail.com>
2013-10-10 06:08:29 -0400

    Renders all posts in database to page

--- main.rb
+++ main.rb
@@ -1,13 +1,17 @@
 require 'pg'
 require 'pry'
 require 'sinatra'
-require 'sinatra-reloader' if development?
+require 'sinatra/reloader' if development?
 
 configure do
-  CONNECTION = PG.connect(dbname 'blog', host: 'localhost')
+  CONNECTION = PG.connect(dbname: 'blog', host: 'localhost')
 end
 
 def query(sql)
   CONNECTION.exec(sql)
 end
 
+get '/posts' do 
+  @posts = query("SELECT * FROM posts")
+  erb :posts
+end
