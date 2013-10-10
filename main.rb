bdc87962cb9e48d1d333a86729be8f5970fb8048
Blake Ruddock <ruddock.blake@gmail.com>
2013-10-09 20:42:49 -0400

    Blake and Ben final commit. Nice styling huh?

--- main.rb
+++ main.rb
@@ -15,7 +15,27 @@ end
 
 
 get '/blog' do 
-	@entries = query("SELECT * FROM blog")
-	erb :blog_index
+  @entries = query("SELECT * FROM blog")
+  erb :blog_index
 end
 
+post '/blog/entry' do 
+  @id = params[:id].to_s
+  
+  @subjecta = query("SELECT subject FROM blog WHERE id = '#{@id}'")
+  @subjecta.each do |subject|
+    @subject = subject["subject"]
+  end
+  
+  @contenta = query("SELECT contents FROM blog WHERE id = '#{@id}'")
+  @contenta.each do |content|
+  @content = content["contents"]
+  end
+
+  @createda = query("SELECT created_at FROM blog WHERE id = '#{@id}'")
+  @createda.each do |date|
+  @created = date["created_at"]
+  end
+
+  erb :blog_entry
+end
