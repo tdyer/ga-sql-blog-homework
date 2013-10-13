458ab30796606c6a2259e1fb68a27ad0459fd026
Rahul Horé <hore.rahul@gmail.com>
2013-10-13 19:26:43 -0400

    Creates posts

--- main.rb
+++ main.rb
@@ -11,24 +11,29 @@ def query(sql)
   CONNECTION.exec(sql)
 end
 
-# show all posts on one page
+# show all posts index page
 get '/posts' do 
   @posts = query("SELECT * FROM posts")
   erb :post_index
 end
 
 # show the form to create new posts
-get 'posts/new' do 
+get '/posts/new' do 
   erb :post_new
 end
 
-# 
-# post '/posts/create' do 
-# end
+# submit new post and show created post on index page
+post '/posts/create' do 
+  title = params[:title]
+  content = params[:content]
+  query("INSERT INTO posts (subject, contents) VALUES ('#{title}', '#{content}')")
+  redirect '/posts'
+end
 
-# show a single post on one page
+# show a single post on its own page
 get '/posts/:id' do
   post_id = params[:id]
   @post = query("SELECT * FROM posts WHERE id = #{post_id} LIMIT 1").first
   erb :post_show
 end
+
