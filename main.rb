03ffba8d06768d0c36095014fc2595f0f5f68faa
Rahul Horé <hore.rahul@gmail.com>
2013-10-14 09:56:51 -0400

    Updates posts

--- main.rb
+++ main.rb
@@ -17,6 +17,13 @@ get '/posts' do
   erb :post_index
 end
 
+# show the form to edit a given post
+get '/posts/:id/edit' do
+  post_id = params[:id]
+  @post = query("SELECT * FROM posts WHERE id = #{post_id} LIMIT 1").first
+  erb :post_edit
+end
+
 # show the form to create new posts
 get '/posts/new' do 
   erb :post_new
@@ -33,6 +40,15 @@ end
 post '/posts/create' do 
   title = params[:title]
   content = params[:content]
-  query("INSERT INTO posts (subject, contents) VALUES ('#{title}', '#{content}', CURRENT_DATE)")
+  query("INSERT INTO posts (subject, contents, created_at) VALUES ('#{title}', '#{content}', CURRENT_DATE)")
   redirect '/posts'
+end
+
+# submit edited post and show updated post on index page
+post '/posts/:id/update' do
+  post_id = params[:id]
+  title = params[:title]
+  content = params[:content]
+  query("UPDATE posts SET subject='#{title}', contents='#{content}' WHERE id=#{post_id}")
+  redirect "posts/#{post_id}"
 end
